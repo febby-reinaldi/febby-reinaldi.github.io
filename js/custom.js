f43a54666e9d73a47054691fe5c250ad85f61ef6
febby-reinaldi <167209505+febby-reinaldi@users.noreply.github.com>
2024-04-17 17:23:16 +0700

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -80,14 +80,14 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("dates").innerHTML = "<div class='col-md-3 col-sm-3'><p>" + days + "<br>Hari</p></div>" + "<div class='col-md-3 col-sm-3'><p>" + hours + "<br>Jam</p></div>"
-  + "<div class='col-md-3 col-sm-3'><p>" + minutes + "<br>Menit</p></div>" + "<div class='col-md-3 col-sm-3'><p>" + seconds + "<br>Detik</p></div>";
+  document.getElementById("dates").innerHTML = "<div class='col-3 col-md-3 col-sm-3'><p>" + days + "<br>Hari</p></div>" + "<div class='col-3 col-md-3 col-sm-3'><p>" + hours + "<br>Jam</p></div>"
+  + "<div class='col-3 col-md-3 col-sm-3'><p>" + minutes + "<br>Menit</p></div>" + "<div class='col-3 col-md-3 col-sm-3'><p>" + seconds + "<br>Detik</p></div>";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("dates").innerHTML = "<div class='col-md-3 col-sm-3'><p>" + 0 + "<br>Hari</p></div>" + "<div class='col-md-3 col-sm-3'><p>" + 0 + "<br>Jam</p></div>"
-	+ "<div class='col-md-3 col-sm-3'><p>" + 0 + "<br>Menit</p></div>" + "<div class='col-md-3 col-sm-3'><p>" + 0 + "<br>Detik</p></div>";
+    document.getElementById("dates").innerHTML = "<div class='col-3 col-md-3 col-sm-3'><p>" + 0 + "<br>Hari</p></div>" + "<div class='col-3 col-md-3 col-sm-3'><p>" + 0 + "<br>Jam</p></div>"
+	+ "<div class='col-3 col-md-3 col-sm-3'><p>" + 0 + "<br>Menit</p></div>" + "<div class='col-3 col-md-3 col-sm-3'><p>" + 0 + "<br>Detik</p></div>";
   }
 },
 1000)
